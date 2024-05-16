4979b5e0438d53cedf12cfaa0c334bb6aa6d196a
Davinder Singh <dasingh9@gmail.com>
2024-05-16 22:06:00 +1200

    Add divide function

--- arithmetic-calculator.js
+++ arithmetic-calculator.js
@@ -15,3 +15,18 @@ function multiplyNumbers() {
     let result = num1 * num2;
     document.getElementById("result").innerText = result;
 }
+
+function divideNumbers() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if(num2 == 0) {
+        const errorMessage = "Invalid value for denominator";
+        console.error(errorMessage);
+    }
+
+    let result = num1 / num2;
+    document.getElementById("result").innerText = result;
+}
